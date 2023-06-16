50d559420e6b7d0226da905dff5d59fc232c7887
Talisson Barbosa <talissonbarbosa@gmail.com>
2023-06-16 18:57:31 -0300

    fazendo setup do projeto

---
+++ src/scripts/main.js
@@ -0,0 +1,19 @@
+AOS.init();
+
+const dataDoEvento = new Date('Feb 01, 2024 20:00:00')
+
+const timeStampDoEvento = dataDoEvento.getTime()
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date()
+    const timeStampAtual = agora.getTime()
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual
+    const dias = Math.floor(distanciaAteOEvento / (1000 * 60 *60 * 24))
+    const horas = Math.floor((distanciaAteOEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 *60))
+    const minutos = Math.floor((distanciaAteOEvento % (1000 * 60 *60)) / (1000 * 60))
+    const segundos = Math.floor((distanciaAteOEvento % (1000 * 60) ) / 1000)
+    
+
+    document.getElementById('dataAniversario').innerHTML =  `${dias}d ${horas}h ${minutos}m ${segundos}s`;
+},1000)
